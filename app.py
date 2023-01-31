9c130a6a2f518ebcbfbea22fda7db54dcf16ff31
Paulo Alexandre Regis <regisin@gmail.com>
2023-01-31 15:20:05 -0600

    banana.dev maybe?

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 
 import argparse
 import os
+import io
 
 import torch
 import torchaudio
 
+from scipy.io import wavfile
+
 from api import TextToSpeech
-from utils.audio import load_voices
+from utils.audio import load_voices, mp3_bytes_from_wav_bytes
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
@@ -21,43 +24,39 @@ def init():
 def inference(model_inputs:dict) -> dict:
     global model
 
-    # Parse out your arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--text', type=str, help='Text to speak.', default="The expressiveness of autoregressive transformers is literally nuts! I absolutely adore them.")
-    parser.add_argument('--voice', type=str, help='Selects the voice to use for generation. See options in voices/ directory (and add your own!) '
-                                                 'Use the & character to join two voices together. Use a comma to perform inference on multiple voices.', default='random')
-    parser.add_argument('--preset', type=str, help='Which voice preset to use.', default='fast')
-    parser.add_argument('--output_path', type=str, help='Where to store outputs.', default='results/')
-    parser.add_argument('--model_dir', type=str, help='Where to find pretrained model checkpoints. Tortoise automatically downloads these to .models, so this'
-                                                      'should only be specified if you have custom checkpoints.', default=MODELS_DIR)
-    parser.add_argument('--candidates', type=int, help='How many output candidates to produce per-voice.', default=3)
-    parser.add_argument('--seed', type=int, help='Random seed which can be used to reproduce results.', default=None)
-    parser.add_argument('--produce_debug_state', type=bool, help='Whether or not to produce debug_state.pth, which can aid in reproducing problems. Defaults to true.', default=True)
-    parser.add_argument('--cvvp_amount', type=float, help='How much the CVVP model should influence the output.'
-                                                          'Increasing this can in some cases reduce the likelihood of multiple speakers. Defaults to 0 (disabled)', default=.0)
-    args = parser.parse_args()
 
 
-
-
-
+    text = model_inputs.get('text', None)
+    voice = model_inputs.get('voice', 'random')
+    preset = model_inputs.get('preset', 'fast')
+    if voice == 'custom':
+        wavBytesString = model_inputs.get('wavBytesString', None)
+        if wavBytesString == None:
+            return {'message': "Custom voice requires audio sample"}
     
-    text = model_inputs.get('text', None)
-    voice = model_inputs.get('voice', None)
-    preset = model_inputs.get('preset', None)
-    text = model_inputs.get('text', None)
-    text = model_inputs.get('text', None)
-    text = model_inputs.get('text', None)
-    if prompt == None:
-        return {'message': "No prompt provided"}
+        wavBytes = BytesIO(base64.b64decode(wavBytesString.encode("ISO-8859-1")))
+        
+        custom_voice_folder = f"tortoise/voices/{voice}"
+        os.makedirs(custom_voice_folder)
+        with open(os.path.join(custom_voice_folder, 'input.wav'), 'wb') as f:
+            f.write(wavBytes.getbuffer())
+
+    voice_samples, conditioning_latents = load_voice(voice)
     
     # Run the model
-    voice_samples, conditioning_latents = load_voice(CUSTOM_VOICE_NAME)
-gen = tts.tts_with_preset(text, voice_samples=voice_samples, conditioning_latents=conditioning_latents, 
+    gen = model.tts_with_preset(text, voice_samples=voice_samples, conditioning_latents=conditioning_latents, 
                           preset=preset)
-torchaudio.save(f'generated-{CUSTOM_VOICE_NAME}.wav', gen.squeeze(0).cpu(), 24000)
-IPython.display.Audio(f'generated-{CUSTOM_VOICE_NAME}.wav')
-    result = model(prompt)
+
+    if voice == 'custom':
+        custom_voice_folder = f"tortoise/voices/{voice}"
+        os.remove(os.path.join(custom_voice_folder, 'input.wav'))
+    
+    wav_bytes = io.BytesIO()
+    wavfile.write(wav_bytes, 24000, gen.squeeze(0).cpu().numpy())
+    wav_bytes.seek(0)
+    mp3Bytes = mp3_bytes_from_wav_bytes()
+
+    result = { audio: "data:audio/mpeg;base64," + base64_encode(mp3_bytes)}
 
     # Return the results as a dictionary
     return result
